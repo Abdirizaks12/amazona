8647b40706b5a008cd2cce7b8538dfad1f7001a4
Abdirizak <abdirizaksigat99@gmail.com>
2022-12-30 06:53:56 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import data from './data';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header>
+        <a>amazona</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/{product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/{product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
