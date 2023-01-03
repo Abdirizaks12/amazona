75c8d7d2f1ace181089d771b9c9ae482a30593d4
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-03 01:54:53 -0800

    fix: routing bugs

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -9,11 +9,11 @@ export default function HomeScreen() {
       <div className="products">
         {data.products.map((product) => (
           <div className="product" key={product.slug}>
-            <Link to={`/product/{product.slug}`}>
+            <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </Link>
             <div className="product-info">
-              <Link to={`/product/{product.slug}`}>
+              <Link to={`/product/${product.slug}`}>
                 <p>{product.name}</p>
               </Link>
               <p>
