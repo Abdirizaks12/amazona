408c5a3870bfa291108c9d729bedc4e683281474
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-19 20:10:59 -0800

    fixed rendering issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,35 @@ import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
+    // <BrowserRouter>
+    //   <div>
+    //     <header>
+    //       <Link to="/">amazona</Link>
+    //     </header>
+    //     <main>
+    //       <Routes>
+    //         <Route path="/product/:slug" element={<ProductScreen />} />
+    //         <Route path="/" element={<HomeScreen />} />
+    //       </Routes>
+    //     </main>
+    //   </div>
+    // </BrowserRouter>
+
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import Axios from 'axios';
 import { useReducer } from 'react';
 import logger from 'use-reducer-logger';
 
@@ -30,8 +30,8 @@ export default function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get('http://localhost:5000/api/products');
-        dispatch({ type: 'FETCH_SUCCESs', payload: result.data });
+        const result = await Axios.get('http://localhost:5000/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
@@ -43,23 +43,70 @@ export default function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => {
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>;
-        })}
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => {
+            return (
+              <div className="product" key={product.slug}>
+                <Link to={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </Link>
+                <div className="product-info">
+                  <Link to={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </Link>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
 }
+
+// import { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+// // import data from '../data';
+
+// export default function HomeScreen() {
+//   const [products, setProducts] = useState([]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const result = await axios.get('http://localhost:5000/api/products');
+//       setProducts(result.data);
+//     };
+//     fetchData();
+//   }, []);
+//   return (
+//     <div>
+//       <h1>Featured Products</h1>
+//       <div className="products">
+//         {products.map((product) => (
+//           <div className="product" key={product.slug}>
+//             <Link to={`/product/${product.slug}`}>
+//               <img src={product.image} alt={product.name} />
+//             </Link>
+//             <div className="product-info">
+//               <Link to={`/product/${product.slug}`}>
+//                 <p>{product.name}</p>
+//               </Link>
+//               <p>
+//                 <strong>${product.price}</strong>
+//               </p>
+//               <button>Add to cart</button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
